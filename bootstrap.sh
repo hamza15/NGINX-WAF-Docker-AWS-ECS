36e44110cc791be0d4b7c7228d5e045526a31ab0
Sardar Hamza Naqi <sardan5@vt.edu>
2018-09-24 14:05:10 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,7 +48,31 @@ tar -xzvf v3.0.2.tar.gz
 mv owasp-modsecurity-crs-3.0.2 /usr/local
 cd /usr/local/owasp-modsecurity-crs-3.0.2
 
-#
+#Copy example file to a config file that you can use for security rules.
+cp crs-setup.conf.example crs-setup.conf
 
+#Remove pre-existing config files.
+rm /etc/nginx/modsec/main.conf -rf
+rm /etc/nginx/modsec/modsecurity.conf -rf
+rm /etc/nginx/nginx.conf -rf
+rm /etc/nginx/conf.d/default.conf -rf
+
+#Copy your own config files from AWS S3 bucket assuming your container's host has access to AWS S3 via IAM roles.
+aws s3 cp s3://path/to/your/main.conf /etc/nginx/modsec/main.conf
+aws s3 cp s3://path/to/your/modsecurity.conf /etc/nginx/modsec/modsecurity.conf
+aws s3 cp s3://path/to/your/nginx.conf /etc/nginx/nginx.conf
+aws s3 cp s3://path/to/your/cert/ServerCert.pem /etc/nginx/ssl/ServerCert.pem
+aws s3 cp s3://path/to/your/allowed/internal_ips.conf /etc/nginx/internal_ips.conf
+
+#Create directory for dhparams and public key infrastructure (pki)
+mkdir /etc/pki/nginx
+chmod 666 /etc/pki/*
+openssl dhparam -out /etc/pki/nginx/dhparams.pem 2048
+
+#Start nginx service
+service nginx start
+
+#start nginx at boot
+update-rc.d nginx defaults
 
 
